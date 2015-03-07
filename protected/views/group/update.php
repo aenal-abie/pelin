6fbecf6ba61c54f88fb42ac1197612b506925881
Zaenal Abidin <aenal.abie@gmail.com>
2015-03-07 17:52:48 +0800

    update surce update

--- protected/views/group/update.php
+++ protected/views/group/update.php
@@ -5,13 +5,6 @@ $this->breadcrumbs = array(
     'Update',
 );
 
-
-//$this->menu = array(
-//    array('label' => 'List Group', 'url' => array('index')),
-//    array('label' => 'Create Group', 'url' => array('create')),
-//    array('label' => 'View Group', 'url' => array('view', 'id' => $model->id)),
-//    array('label' => 'Manage Group', 'url' => array('admin')),
-//);
 ?>
 
 <?php echo $this->renderPartial('_form', array('model' => $model)); ?>
